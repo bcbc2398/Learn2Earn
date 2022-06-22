a5336c9a711b8a92f11ab1fa035e089d5a957841
bcbc2398 <99771842+bcbc2398@users.noreply.github.com>
2022-06-22 06:55:08 -0400

    Update LearnToken.sol

--- LearnToken.sol
+++ LearnToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Learn is ERC20, ERC20Burnable, Ownable {
+contract LearnToken is ERC20, ERC20Burnable, Ownable {
     uint constant _initial_supply = 1000000000000000000;
     constructor() ERC20("LEARN", "LRN") {
         _mint(msg.sender, _initial_supply);
